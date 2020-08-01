e03e80f83cd00223e36802b4a3a8f474160d6f59
Yeongh-Jeong <jyh9805@gmail.com>
2020-08-01 22:29:27 +0900

    homework

--- .Java/etc/GoogleExam.java
+++ .Java/etc/GoogleExam.java
@@ -3,6 +3,29 @@ class GoogleExam{
 		/*
 		1~1000 사이의 정수중 8의 개수를 구하여 출력
 		*/
-		System.out.println("Hello World!");
+
+		//1. int를 활용한 방법
+		int count = 0;
+		int num;
+		for (int i=1;i<=1000;i++){
+			num = i;
+			while (num!=0){
+				if (num%10==8){count ++;}
+				num /= 10;
+			}
+		}
+
+		System.out.println("8의 개수 : " + count);
+
+
+
+		//2. String을 활용한 방법
+		String seq = "";
+		for (int i=1;i<=1000;i++){
+			seq += i;
+		}
+
+		System.out.println(seq);
+
 	}
 }
